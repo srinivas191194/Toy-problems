007f1f1d96867fcbfe7a6d91f832871897c08595
srinivas191194 <srinivas191194@msitprogram.net>
2020-04-13 22:28:00 +0530

    assignment done

--- lru/lru.py
+++ lru/lru.py
@@ -1,14 +1,36 @@
 class lru:
-  def __init__(self, size, data):
-    self.size = size
-    self.data = data
 
-  def put(self, key, data):
-        
-        pass
-  def get(self, key,default = None):
-        
-        pass
-  def get_cache(self):
-         
-         pass
+    def __init__(self, size):
+        self.size = size
+        self.lru = []
+        self.lruCache = {}
+
+    def get(self, key, default=None):
+        if key in self.lru:
+            return self.lruCache[key]
+        else:
+            return default
+
+    def put(self, key):
+
+        if(len(self.lru) < self.size):
+            if key in self.lru:
+                self.lru.remove(key)
+                self.lru.append(key)
+                self.lruCache[key] = "www."+str(key)+".com"
+            else:
+                self.lru.append(key)
+                self.lruCache[key] = "www."+str(key)+".com"
+        else:
+
+            var = self.lru.pop(0)
+            self.lru.append(key)
+            del self.lruCache[var]
+            self.lruCache[key] = "www."+str(key)+".com"
+
+    def get_cache(self):
+        ls = []
+        for key in reversed(self.lru):
+            ls.append(key+"-" + self.lruCache[key])
+        return ls
+
--- lru/lruTest.py
+++ lru/lruTest.py
@@ -1,26 +1,29 @@
 from lru import lru
 
 
-
 class lruTest:
 
-    def __init__(self):
+    def _init_(self):
         pass
 
     def testcases(self):
-        a = lru(3, "found")
+        a = lru(3)
         a.put("google")
-        assert a.get("google") == "found", "Not found"
+        assert a.get("google") == "www.google.com", "testcase 1 failed"
+        print("testcase 1 passed")
         a.put("facebook")
-        assert a.get("facebook") == "found", "Not found"
+        assert a.get("facebook") == "www.facebook.com", "testcase 2 failed"
+        print("testcase 2 passed")
         a.put("gmail")
         a.put("youtube")
-        assert a.get("google") == None, "Not found"
-        assert a.get_cache() == {"youtube": "found",
-                                 "gmail": "found", "facebook": "found"}
+        assert a.get("google") == None, "testcase 3 failed"
+        print("testcase 3 passed")
+        print("All Testcases passed!!")
+        lst = a.get_cache()
+        for i in lst:
+            print(i)
 
 
 if __name__ == "__main__":
     a = lruTest()
     a.testcases()
-
